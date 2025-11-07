9d3c25b43512d50d4d6fadd612faedcf9bf72f30
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-07 23:41:51 +0530

    Add files via upload

---
+++ Q121.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char name[50];
+    int age;
+
+    fp = fopen("info.txt", "w"); 
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    fprintf(fp, "Name: %s\nAge: %d\n", name, age);
+
+    fclose(fp);
+    printf("File created successfully! Data written to info.txt\n");
+
+    return 0;
+}
