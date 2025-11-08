f55e1ddfacc60135f6492823430414c66da08841
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-08 13:12:25 +0530

    Add files via upload

---
+++ Q122.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    char line[100];
+
+    
+    file = fopen("info.txt", "r");
+
+    
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+
+   
+    fclose(file);
+
+    return 0;
+}
