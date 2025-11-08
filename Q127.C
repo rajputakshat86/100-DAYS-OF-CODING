995f08e8f504233aa2a3120984fb541c3e1e9568
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-08 13:14:59 +0530

    Add files via upload

---
+++ Q127.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *inputFile, *outputFile;
+    char ch;
+
+    
+    inputFile = fopen("input.txt", "r");
+    if (inputFile == NULL) {
+        printf("Error: Cannot open input.txt!\n");
+        return 1;
+    }
+
+    
+    outputFile = fopen("output.txt", "w");
+    if (outputFile == NULL) {
+        printf("Error: Cannot create output.txt!\n");
+        fclose(inputFile);
+        return 1;
+    }
+
+    
+    while ((ch = fgetc(inputFile)) != EOF) {
+        fputc(toupper(ch), outputFile);
+    }
+
+    printf("File converted successfully! Check output.txt\n");
+
+    fclose(inputFile);
+    fclose(outputFile);
+    return 0;
+}
