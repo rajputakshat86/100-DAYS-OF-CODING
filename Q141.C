b9146c64e5438c8acaa0f1b5fcd8fc9d908edade
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-20 21:36:30 +0530

    Add files via upload

---
+++ Q141.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student
+{
+    char name[50];
+    int roll_no;
+    int marks;
+};
+
+int main()
+{
+    struct Student s;
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter Marks: ");
+    scanf("%d", &s.marks);
+
+    printf("Name: %s | Roll: %d | Marks: %d\n", s.name, s.roll_no, s.marks);
+
+    return 0;
+}
